f35543529bd0f2c3ab1913a489fcbc7f72eff8ad
Limbicnation <gero@limbicnation.com>
2019-11-29 21:56:37 +0100

    save sketch

--- sketch_sound_visualization.js
+++ sketch_sound_visualization.js
@@ -9,5 +9,15 @@ function setup() {
 function draw() {
     background(0);
     var vol = mic.getLevel();
+
+    // // Add color
+    // ColorMode(HSB);
+    fill(vol * 5, 204, vol * 5);
+
+    // Draw ellipse
     ellipse(500, 500, 500, vol * 20000);
+
+    // Draw left eye
+    ellipse(100, 100, 500, 500 + vol * 200);
+    
 }
