d5e242fc2b3ed7af3a133f6f9e21cc54c07adf4f
Limbicnation <gero@limbicnation.com>
2019-11-29 03:25:55 +0100

    saved sketch

--- sketch_random.js
+++ sketch_random.js
@@ -1,5 +1,8 @@
 
 let noiseScale=0.02;
+let offset = 1;
+let easing = 0.05
+randomSeed(99);
 
 function setup() {
     var cnv = createCanvas(800, 800);
@@ -14,17 +17,24 @@ function draw(){
         for (let y = 0; y <= height; y = y + 30) {
 
       
-  var circleX = random(width);
-  var circleY = random(height);
-  var circleSize = random(1, 512);
+  var squareX = random(width);
+  var squareY = random(height);
+  var squareSize = random(1, 512);
   noStroke();
-  fill(random(255), random(255), random(255)); 
+  fill((255), random(255), (255)); 
   
   noSmooth();
-  ellipse (random(circleX), random(circleY), random(circleSize))
-  //t = i + i++; // update time
+  ellipse (random(squareX), random(squareY), random(squareSize))
+  //t = 20 + i++; // update time
 
         }
     }
+    
+    for (let i = 1; i <= 1000; i++) {
+        tint(20, 20)
+        let r = random(255, 55, 100, 255);
+        stroke(r);
+        line(i, offset, i++, 1000);
+      }
 
 }
